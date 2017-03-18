d01ea10ea6b393aaf93e39809b9b8a521763b97a
sergby2009 <miror99@mail.ru>
2017-03-19 00:15:22 +0300

    Анализ руки игрока

--- Cardgame/Cardgame.cpp
+++ Cardgame/Cardgame.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include <cstdio>
 #include <locale>
+#include <string.h>
 
 #define COL_CARDS 52
 #define COL_USERS 1
@@ -11,13 +12,16 @@
 #define CNAME 13
 #define CTYPE 4
 #define COL_CARD_INFO 4
+#define COL_CARD_ARM 5
 
 
 void shufleCard(int [][13], int);
 bool getCardsToUser(int [][COL_CARDS][COL_CARD_INFO], int, int, int* , int, int[][13]);
 void printUsersCard(int [][COL_CARDS][COL_CARD_INFO], int, const int [][13], const char* [], const char* []);
 void printUserCard(int [][COL_CARDS][COL_CARD_INFO], int, const int[][13], const char* [], const char* []);
-void userArm_analiz(int[][COL_CARD_INFO], int[][CNAME]);
+void userArm_analiz(int[][COL_CARD_INFO], int[2]);
+
+bool getResult_Double(int tempArr[COL_CARDS][COL_CARD_INFO], int resultArr[]);
 
 void findCardName(int, const char* [], const int [][13], const char* [], const char* []);
 void findCardCoor(int, int*, const int [][13]);
@@ -26,18 +30,22 @@ int  findCardID(const int [][13], int, int);
 int main()
 {
 	setlocale(LC_ALL, "russian");
-	
-	//int cardMap[4][13] = {0};
-	int cardMap[4][13] = {	{1,13,26,25,38,39,6,7,8,9,10,11,12},
+		
+	//int cardMap[4][13] =	{0};
+	/*int cardMap[4][13] =	{{1,13,26,25,38,39,6,7,8,9,10,11,12},
 							{2,14,15,16,17,18,19,20,21,22,23,24,5},
-							{3,27,28,29,30,31,32,33,34,35,36,37,4},
+							{27,3,28,29,30,31,32,33,34,35,36,37,4},
+							{52,40,41,42,43,44,45,46,47,48,49,50,51}};*/
+	int cardMap[4][13]	=	{{1,2,3,4,5,39,6,7,8,9,10,11,12},
+							{13,14,15,16,17,18,19,20,21,22,23,24,38},
+							{27,26,28,29,30,31,32,33,34,35,36,37,25},
 							{52,40,41,42,43,44,45,46,47,48,49,50,51}};
 	int cardsUser[COL_USERS][COL_CARDS][COL_CARD_INFO] = { 0 };
-	char* cardName[13] = {"Туз","Двойка","Тройка","Четверка","Пятерка","Шестерка","Семерка","Восьмерка","Девятка","Десятка","Валет","Дама","Король"};
+	int cardsUserArm[COL_USERS][2] = {0};
+	char* cardName[13] = {"Двойка","Тройка","Четверка","Пятерка","Шестерка","Семерка","Восьмерка","Девятка","Десятка","Валет","Дама","Король","Туз"};
 	char* cardType[4] =  {"Черви","Буби","Крести","Пики"};
 	int currentCard = 1;
 
-		
 	/*Перетасовка карт*/
 	//shufleCard(cardMap, COL_CARDS);
 	
@@ -49,10 +57,11 @@ int main()
 	printUsersCard(cardsUser, COL_USERS, cardMap, (const char**)cardName, (const char**)cardType);
 
 	/*Проверка на пары*/
-	/*for (int i = 0; i <= COL_USERS - 1; i++)
+	for (int i = 0; i <= COL_USERS - 1; i++)
 	{
-		is_coupe(cardsUser[i], cardMap);
-	}*/
+		printf("Анализ карт игрока %d\n", i);
+		userArm_analiz(cardsUser[i], cardsUserArm[i]);
+	}
 	
 	system("PAUSE");
 	return 0;
@@ -115,8 +124,7 @@ void printUsersCard(int cardsUser[][COL_CARDS][COL_CARD_INFO], int countUser, co
 void printUserCard(int cardsUser[][COL_CARDS][COL_CARD_INFO], int cntUser, const int cardMap[][13], const char* cardName[], const char* cardType[])
 {
 	int cnt = 0; 
-	char* arrCard[2] = {"",""};
-	
+	//char* arrCard[2];
 	
 	printf("****** Карты %d игрока ******\n",cntUser+1);
 	while (cardsUser[cntUser][cnt][0] != 0) 
@@ -145,12 +153,175 @@ void findCardName(int cardID, const char* arrCard[], const int cardMap [][13], c
 		}
 }
 
-void userArm_analiz(int arr[][COL_CARD_INFO], int map[][CNAME])
+
+bool getResult_Double(int tempArr[COL_CARDS][COL_CARD_INFO], int resultArr[])
 {
-	int currentCard = 0;
-	int arrCoord[2] = {0};
-	//int result[COL_CARDS][2] = { 0 };
+	int currentCard = 0, counter = 0;
+	int analizArray[COL_CARD_ARM][2] = { 0 };
+	int point = 0, level = 0;
+
+	/*Подготовка массива для анализа результата руки*/
+	while (tempArr[currentCard][0] != 0)
+	{
+		counter = 0;
+		while (tempArr[counter][0] != 0)
+		{
+			if (counter == currentCard)
+			{
+				counter++;
+				continue;
+			}
+			if (tempArr[currentCard][2] == tempArr[counter][2])
+			{
+				if (tempArr[currentCard][3] == 0) tempArr[currentCard][3] = currentCard + 1;
+				if (tempArr[counter][3] == 0) tempArr[counter][3] = currentCard + 1;
+			}
+			counter++;
+		}
+		currentCard++;
+	}
+
+	/*Определение результата руки*/
 	
+	currentCard = 0;
+	while (tempArr[currentCard][0] != 0)
+	{
+		if (tempArr[currentCard][3] == 0)
+		{
+			currentCard++;
+			continue;
+		}
+		analizArray[(tempArr[currentCard][3])][0] += 1;
+		analizArray[(tempArr[currentCard][3])][1] += tempArr[currentCard][2];
+		currentCard++;
+	}
+	for (int i = 0; i < COL_CARD_ARM; i++)
+	{
+		if (level == 0)
+		{
+			switch (analizArray[i][0])
+			{
+				case 2 :	level = 1;
+							point = analizArray[i][1];
+							break;
+				case 3:		level = 3;
+							point = analizArray[i][1];
+							break;
+				case 4:		level = 7;
+							point = analizArray[i][1];
+							break;
+			}
+		}
+		else
+		{
+			switch (analizArray[i][0])
+			{
+				case 2:		
+					switch (level)
+					{
+						case 1: level = 2;
+								point += analizArray[i][1];
+								break;
+						case 3: level = 6;
+								point += analizArray[i][1];
+								break;
+					}
+					break;
+				case 3:		
+					if (level = 1)
+					{
+						level = 6;
+						point += analizArray[i][1];
+					}
+					break;
+			}
+		}
+	}
+	resultArr[0] = level;
+	resultArr[1] = point;
+	return (level)?true:false;
+}
+
+bool getResult_CardIncrease(int tempArr[COL_CARDS][COL_CARD_INFO], int resultArr[])
+{
+	/*Сортировка массива по возрастанию*/
+	int currentCard = 0, cnt = 0;
+	int tmp[COL_CARD_INFO];
+	
+	while (tempArr[currentCard][0] != 0)
+	{
+		cnt = currentCard + 1;
+		while (tempArr[cnt][0] != 0)
+		{
+			if (tempArr[currentCard][2] > tempArr[cnt][2])
+			{
+				tmp[0] = tempArr[currentCard][0];
+				tmp[1] = tempArr[currentCard][1];
+				tmp[2] = tempArr[currentCard][2];
+				tmp[3] = tempArr[currentCard][3];
+
+				tempArr[currentCard][0] = tempArr[cnt][0];
+				tempArr[currentCard][1] = tempArr[cnt][1];
+				tempArr[currentCard][2] = tempArr[cnt][2];
+				tempArr[currentCard][3] = tempArr[cnt][3];
+
+				tempArr[cnt][0] = tmp[0];
+				tempArr[cnt][1] = tmp[1];
+				tempArr[cnt][2] = tmp[2];
+				tempArr[cnt][3] = tmp[3];
+			}
+			cnt++;
+		}
+		currentCard++;
+	}
+	/*Проверка руки на стрит*/
+	bool flag = true;
+	int point = tempArr[0][2];
+	
+	for (int i = 1; tempArr[i][0] != 0; i++)
+	{
+		point += tempArr[i][2];
+		if (tempArr[i][2] -1 != tempArr[i - 1][2])
+		{
+			flag = false;
+			break;
+		}
+	}
+
+	if (flag)
+	{
+		resultArr[0] = 4;
+		resultArr[1] = point;
+	}
+	else
+	{
+		resultArr[0] = 0;
+		resultArr[1] = 0;
+	}
+
+	return flag;
+}
+
+void userArm_analiz(int arr[COL_CARDS][COL_CARD_INFO], int arm[2])
+{
+	bool result = false, variant1, variant2, variant3, variant4;
+	int tempArr[COL_CARDS][COL_CARD_INFO];
+	int resultVariant[4][2];
+	
+	memcpy(tempArr, arr, sizeof(tempArr));
+	
+	variant1 = getResult_Double(tempArr, resultVariant[0]);
+	printf("Тип руки:%d\tКол-во очков:%d\n", resultVariant[0][0], resultVariant[0][1]);
+
+	variant2 = getResult_CardIncrease(tempArr, resultVariant[1]);
+	printf("Тип руки:%d\tКол-во очков:%d\n", resultVariant[1][0], resultVariant[1][1]);
+	
+
+	/*int currentCard = 0;
+	int arrCoord[2] = {0};*/
+			
+	//int result[COL_CARDS][2] = { 0 };
+		
 	/*Формирование матрицы повторений*/
 
 
@@ -178,7 +349,6 @@ void userArm_analiz(int arr[][COL_CARD_INFO], int map[][CNAME])
 		}
 		currentCard++;
 	}*/
-	printf("stop\n");
 }
 
 int findCardID(const int map[][13], int row, int col)
